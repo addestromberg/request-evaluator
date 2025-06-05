90ca964507be317b1f2c76c095fd0270e0797e2e
Adde Stromberg <adde@upperfield.se>
2025-06-05 08:47:41 +0200

    Update RequestEvaluator to version 1.1.0, enhance configuration with additional parameters, and improve API response handling

--- src/Evaluator/TA/request_evaluator/request_evaluator.py
+++ src/Evaluator/TA/request_evaluator/request_evaluator.py
@@ -5,6 +5,7 @@ import octobot_commons.enums as enums
 import octobot_evaluators.evaluators as evaluators
 import octobot_evaluators.util as evaluators_util
 import octobot_trading.api as trading_api
+import tulipy
 
 
 class RequestEvaluator(evaluators.TAEvaluator):
@@ -14,11 +15,31 @@ class RequestEvaluator(evaluators.TAEvaluator):
     BASIC_AUTH = "basic_auth"
     BASIC_AUTH_USERNAME = "auth_username"
     BASIC_AUTH_PASSWORD = "auth_password"
+    SCORE_FIELD= "score_field"
+    RSI_PERIOD = "rsi_period"
+    BB_PERIOD = "bb_period"
+    BB_STD = "bb_std"
+    MACD_FAST = "macd_fast"
+    MACD_SLOW = "macd_slow"
+    MACD_SIGNAL = "macd_signal"
+    ATR_PERIOD = "atr_period"
+    SHORT_MA_PERIOD = "short_ma_period"
+    LONG_MA_PERIOD = "long_ma_period"
 
     def __init__(self, tentacles_setup_config):
         super().__init__(tentacles_setup_config)
-        self.history_length = 30
+        self.history_length = 60
         self.timeout = 5.0
+        self.score_field = "score"
+        self.rsi_period = 14
+        self.bb_period = 20
+        self.bb_std = 2
+        self.macd_fast = 12
+        self.macd_slow = 26
+        self.macd_signal = 9
+        self.atr_period = 5
+        self.short_ma_period = 20
+        self.long_ma_period = 50
 
     def init_user_inputs(self, inputs: dict) -> None:
         """
@@ -74,6 +95,103 @@ class RequestEvaluator(evaluators.TAEvaluator):
             title="Request Timeout",
         )
 
+        self.rsi_period = self.UI.user_input(
+            self.RSI_PERIOD,
+            enums.UserInputTypes.INT,
+            default_config["rsi_period"],
+            inputs,
+            min_val=1,
+            max_val=100,
+            title="Stochastic RSI Period",
+        )
+
+        self.bb_period = self.UI.user_input(
+            self.BB_PERIOD,
+            enums.UserInputTypes.INT,
+            default_config["bb_period"],
+            inputs,
+            min_val=1,
+            max_val=100,
+            title="Bollinger Band Period",
+        )
+
+        self.bb_std = self.UI.user_input(
+            self.BB_STD,
+            enums.UserInputTypes.INT,
+            default_config["bb_std"],
+            inputs,
+            min_val=0,
+            max_val=100,
+            title="Bollinger Band Multiplier",
+        )
+
+        self.score_field = self.UI.user_input(
+            self.SCORE_FIELD,
+            enums.UserInputTypes.TEXT,
+            default_config["score_field"],
+            inputs,
+            show_in_summary=False,
+            show_in_optimizer=False,
+            title="Score Field",
+        )
+
+        self.macd_fast = self.UI.user_input(
+            self.MACD_FAST,
+            enums.UserInputTypes.INT,
+            default_config["macd_fast"],
+            inputs,
+            min_val=0,
+            max_val=100,
+            title="MACD Fast",
+        )
+        self.macd_slow = self.UI.user_input(
+            self.MACD_SLOW,
+            enums.UserInputTypes.INT,
+            default_config["macd_slow"],
+            inputs,
+            min_val=0,
+            max_val=100,
+            title="MACD Slow",
+        )
+
+        self.macd_signal = self.UI.user_input(
+            self.MACD_SIGNAL,
+            enums.UserInputTypes.INT,
+            default_config["macd_signal"],
+            inputs,
+            min_val=0,
+            max_val=100,
+            title="MACD Signal",
+        )
+
+        self.atr_period = self.UI.user_input(
+            self.ATR_PERIOD,
+            enums.UserInputTypes.INT,
+            default_config["atr_period"],
+            inputs,
+            min_val=0,
+            max_val=100,
+            title="ATR Period",
+        )
+        self.short_ma_period = self.UI.user_input(
+            self.SHORT_MA_PERIOD,
+            enums.UserInputTypes.INT,
+            default_config["short_ma_period"],
+            inputs,
+            min_val=0,
+            max_val=100,
+            title="Short MA Period",
+        )
+        self.long_ma_period = self.UI.user_input(
+            self.LONG_MA_PERIOD,
+            enums.UserInputTypes.INT,
+            default_config["long_ma_period"],
+            inputs,
+            min_val=0,
+            max_val=100,
+            title="Long MA Period",
+        )
+
     @classmethod
     def get_default_config(
         cls,
@@ -82,7 +200,17 @@ class RequestEvaluator(evaluators.TAEvaluator):
         basic_auth: bool = False,
         username: typing.Optional[str] = None,
         password: typing.Optional[str] = None,
-        timeout: float = 2.0
+        timeout: float = 2.0,
+        rsi_period: int = 14,
+        bb_period: int = 20,
+        bb_std: int = 2,
+        macd_fast: int = 12,
+        macd_slow: int = 26,
+        macd_signal: int = 9,
+        atr_period: int = 5,
+        short_ma_period: int = 20,
+        long_ma_period: int = 50,
+        score_field: str = "score"
     ):
         return {
             cls.HISTORY_LENGTH: history_length or 2,
@@ -90,7 +218,17 @@ class RequestEvaluator(evaluators.TAEvaluator):
             cls.BASIC_AUTH: basic_auth or False,
             cls.BASIC_AUTH_USERNAME: username or "",
             cls.BASIC_AUTH_PASSWORD: password or "",
-            cls.TIMEOUT:timeout or 2.0
+            cls.TIMEOUT:timeout or 2.0,
+            cls.RSI_PERIOD:rsi_period or 14,
+            cls.BB_PERIOD:bb_period or 20,
+            cls.BB_STD:bb_std or 2,
+            cls.MACD_FAST:macd_fast or 12,
+            cls.MACD_SLOW:macd_slow or 26,
+            cls.MACD_SIGNAL:macd_signal or 9,
+            cls.ATR_PERIOD:atr_period or 5,
+            cls.SHORT_MA_PERIOD:short_ma_period or 20,
+            cls.LONG_MA_PERIOD:long_ma_period or 50,
+            cls.SCORE_FIELD:score_field or "score"
         }
 
     async def ohlcv_callback(
@@ -104,6 +242,14 @@ class RequestEvaluator(evaluators.TAEvaluator):
         inc_in_construction_data,
     ):
         
+        # We want to pass all the data.
+        timestamp = trading_api.get_symbol_time_candles(
+            self.get_exchange_symbol_data(exchange, exchange_id, symbol),
+            time_frame,
+            limit=self.history_length,
+            include_in_construction=inc_in_construction_data,
+        )
+
         open = trading_api.get_symbol_open_candles(
             self.get_exchange_symbol_data(exchange, exchange_id, symbol),
             time_frame,
@@ -135,7 +281,7 @@ class RequestEvaluator(evaluators.TAEvaluator):
             include_in_construction=inc_in_construction_data,
         )
 
-        candle_data = {"open": open, "high": high, "low": low, "close": close, "volume": volume}
+        candle_data = {"timestamp": timestamp, "open": open, "high": high, "low": low, "close": close, "volume": volume}
 
         await self.evaluate(cryptocurrency, symbol, time_frame, candle_data, candle)
 
@@ -152,16 +298,30 @@ class RequestEvaluator(evaluators.TAEvaluator):
         else:
             client = httpx.AsyncClient(timeout=to)
 
+        # Pass selected indicators
+        rsi = tulipy.stochrsi(real=candle_data.get("close"), period=self.rsi_period)
+        bbands_lower, bbands_middle, bbands_upper = tulipy.bbands(real=candle_data.get("close"),period=self.bb_period, stddev=self.bb_std)
+        macd, signal, hist = tulipy.macd(real=candle_data.get("close"), short_period=self.macd_fast, long_period=self.macd_slow, signal_period=self.macd_signal)
+        atr = tulipy.atr(high=candle_data.get("high"), low=candle_data.get("low"), close=candle_data.get("close"), period=self.atr_period)
+        short_ma = tulipy.sma(real=candle_data.get("close"), period=self.short_ma_period)
+        long_ma = tulipy.sma(real=candle_data.get("close"), period=self.long_ma_period)
+
         payload = {
-            "symbol": self.symbol,
+            "symbol": symbol,
             "time_frame": time_frame,
             "last_candle": candle,
             "history": candle_data,
+            "stochastic_rsi": rsi,
+            "bollinger_band": {"upper": bbands_upper, "middle": bbands_middle, "lower": bbands_lower},
+            "macd": {"macd": macd, "signal": signal, "hist": hist},
+            "atr": atr,
+            "moving_avg": {f"ma_{self.short_ma_period}": short_ma, f"ma_{self.long_ma_period}": long_ma}
         }
+
         try:
             response = await client.post(str(self.url), json=serialize_payload(payload))
             response.raise_for_status()
-            score = float(response.json().get("score", 0))
+            score = float(response.json().get(self.score_field, 0))
             self.eval_note = max(-1.0, min(score, 1.0))
 
         except Exception as e:
